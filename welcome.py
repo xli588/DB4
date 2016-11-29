ed9c3abd394ee348a758b608fd5a728595f49236
xli588 <xli588@uwo.ca>
2016-11-29 17:36:55 -0500

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html> 
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<table border="1">
+<tr>
+<td>Table</td>
+<td>Login as a staff </td>
+<td>Login as a guest</td>
+</tr>
+
+</table>
+</body>
+</html>
