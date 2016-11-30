de0a1e3203e0d794f62e9fc4a0f3551b5412124b
xli588 <xli588@uwo.ca>
2016-11-29 21:46:49 -0500

    Add files via upload

---
+++ init.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('welcome.html')
+
+@app.route("/welcome")
+def welcome():
+    return render_template('staff.html')
+def welcome():
+    return render_template('guest.html')
+
+@app.route("/staff")
+def staff():
+    return render_template('Movie.html')	
+
+@app.route("/movie")
+def hello():
+    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    cursor = cnx.cursor()
+    query = ("SELECT * from Movie")
+    cursor.execute(query)
+    users=cursor.fetchall()
+    cnx.close()
+    return render_template('users.html',users=users)
+
+@app.route('/entername')
+def helloName(name=None):
+    return render_template('form.html', name=name)
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    cursor = cnx.cursor()
+    insert_stmt = (
+        "INSERT INTO Movies (MovieID, MovieName, MovieYear) "
+        "VALUES (%s, %s, %s)"
+    )
+    data = (request.form['Movie ID'], request.form['Movie Name'], request.form['Movie Year'])
+    cursor.execute(insert_stmt, data)
+    cnx.commit()
+    cnx.close()
+    return render_template('index.html', MovieName=request.form['Movie Name'])
+
+@app.route('/sqlInjection')
+def sqlInjection(name=None):
+    return render_template('form2.html')
+
+@app.route('/submitSqlInjection', methods=["POST"])
+def sqlInjectionResult():
+    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    cursor = cnx.cursor()
+
+    firstName = request.form['firstname']
+    query = ("SELECT * from Customer where firstname = '" + firstName + "'")
+    cursor.execute(query)
+    print("Attempting: " + query)
+    users=cursor.fetchall()
+
+    cnx.commit()
+    cnx.close()
+    return str(users)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
