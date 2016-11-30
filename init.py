3030f1c12a157ced738cc6677a74007b474118b6
xli588 <xli588@uwo.ca>
2016-11-30 16:18:44 -0500

    Add files via upload

--- init.py
+++ init.py
@@ -27,35 +27,76 @@ def Movie():
     cnx.close()
     return render_template('Movie.html',users=users)
 
+
 @app.route("/Genre")
+
 def Genre():
-    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
-    cursor = cnx.cursor()
-    query = ("SELECT * from Genre")
-    cursor.execute(query)
-    users=cursor.fetchall()
-    cnx.close()
-    return render_template('Genre.html',users=users)
+    
+   cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    
+   cursor = cnx.cursor()
+    
+   query = ("SELECT * from Genre")
+    
+   cursor.execute(query)
+    
+   users=cursor.fetchall()
+    
+   cnx.close()
+    
+   return render_template('Genre.html',users=users)
 
 
 @app.route("/Showing")
-def Showing():
-    cnx = mysql.connector.connect(user='root', database='MovieTheatre')
-    cursor = cnx.cursor()
-    query = ("SELECT * from Showing")
-    cursor.execute(query)
-    users=cursor.fetchall()
-    cnx.close()
-    return render_template('Showing.html',users=users)
 
+def Showing():
+    
+   cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    
+   cursor = cnx.cursor()
+    
+   query = ("SELECT * from Showing")
+    
+   cursor.execute(query)
+    
+   users=cursor.fetchall()
+    
+   cnx.close()
+    
+   return render_template('Showing.html',users=users)
+
+
+@app.route("/Customer")
+
+def Customer():
+    
+   cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    
+   cursor = cnx.cursor()
+    
+   query = ("SELECT * from Customer")
+    
+   cursor.execute(query)
+    
+   users=cursor.fetchall()
+    
+   cnx.close()
+    
+   return render_template('Customer.html',users=users)
 
 
 @app.route("/Attend")
+
 def Attend():
-   cnx = mysql.connector.connect(user='root', database='MovieTheatre') 
+    
+   cnx = mysql.connector.connect(user='root', database='MovieTheatre')
+    
    cursor = cnx.cursor()
+    
    query = ("SELECT * from Attend")
-   cursor.execute(query) 
+    
+   cursor.execute(query)
+    
    users=cursor.fetchall()
     
    cnx.close()
